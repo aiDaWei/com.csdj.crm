3ab046bd1597c5e399f686b8fb41060cad462fde
xiaojiang2000 <1773603105@qq.com>
2020-02-21 14:36:22 +0800

    增删改查

--- src/main/java/com/csdj/crmproject/crmproject/service/Marketing/ActivityMarketing/impl/ActivityMarketingServiceImpl.java
+++ src/main/java/com/csdj/crmproject/crmproject/service/Marketing/ActivityMarketing/impl/ActivityMarketingServiceImpl.java
@@ -12,8 +12,34 @@ public class ActivityMarketingServiceImpl implements ActivityMarketingService {
     @Resource
     private ActivityMarketingMapper ActivityMarketingMapper;
 
+
     @Override
     public List<MarketActivity> activi() {
         return ActivityMarketingMapper.activi();
+    }
+
+    @Override
+    public int marketCount() {
+        return ActivityMarketingMapper.marketCount();
+    }
+
+    @Override
+    public int addMarket(MarketActivity activity) {
+        return ActivityMarketingMapper.addMarket(activity);
+    }
+
+    @Override
+    public int delMarket(int pkMarkActivityId) {
+        return ActivityMarketingMapper.delMarket(pkMarkActivityId);
+    }
+
+    @Override
+    public MarketActivity uppkMarkActivityId(int pkMarkActivityId) {
+        return ActivityMarketingMapper.uppkMarkActivityId(pkMarkActivityId);
+    }
+
+    @Override
+    public int updaMarket(MarketActivity activity) {
+        return ActivityMarketingMapper.updaMarket(activity);
     }
 }
